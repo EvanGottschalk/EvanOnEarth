84551490d3e2fb5c2698b8c708dbc07e8a2dbaa6
EvanGottschalk <magnus5557@gmail.com>
2024-04-27 19:35:16 -0400

    updated about and footer

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -32,7 +32,31 @@ const About = () => {
           <a data-aos="flip-down" data-aos-delay="0" href='https://evanon.earth/nofunlabs' target="_blank" 
           className='aboutText aboutLink' id='nofunlabsLink' onMouseOver={mouseover} onMouseLeave={mouseleave} style={{
             textDecoration: 'none',
-            color: '#7CE2F9'}}>evanon.earth/nofunlabs -></a>
+            color: '#7CE2F9'}}>Learn More -></a>
+          <br></br>
+          <div className='aboutText aboutTitle' data-aos="fade-right" data-aos-delay="100">Web3 Engineer at Aphid AI</div>
+          <div className='aboutText aboutTagline' data-aos="fade-right" data-aos-delay="200" style={{
+            textDecoration: 'none',
+            color: '#00F8BE'}}>- Decentralized Bot Marketplace -</div>
+          <div className='aboutText' data-aos="fade-right" data-aos-delay="300" style={{
+            textDecoration: 'none',
+            color: '#bbbbbb'}}>Start onboarding your next 1,000,000 users now:</div>
+          <a data-aos="flip-down" data-aos-delay="0" href='https://evanon.earth/nofunlabs' target="_blank" 
+          className='aboutText aboutLink' id='nofunlabsLink' onMouseOver={mouseover} onMouseLeave={mouseleave} style={{
+            textDecoration: 'none',
+            color: '#7CE2F9'}}>Learn More -></a>
+          <br></br>
+          <div className='aboutText aboutTitle' data-aos="fade-right" data-aos-delay="100">Blockchain Architect at EcoVerse</div>
+          <div className='aboutText aboutTagline' data-aos="fade-right" data-aos-delay="200" style={{
+            textDecoration: 'none',
+            color: '#ea49ff'}}>- Blockchain Onboarding Solutions -</div>
+          <div className='aboutText' data-aos="fade-right" data-aos-delay="300" style={{
+            textDecoration: 'none',
+            color: '#bbbbbb'}}>Start onboarding your next 1,000,000 users now:</div>
+          <a data-aos="flip-down" data-aos-delay="0" href='https://evanon.earth/nofunlabs' target="_blank" 
+          className='aboutText aboutLink' id='nofunlabsLink' onMouseOver={mouseover} onMouseLeave={mouseleave} style={{
+            textDecoration: 'none',
+            color: '#7CE2F9'}}>Learn More -></a>
           <br></br>
           <div data-aos="fade-right"  data-aos-delay="500" className='aboutText aboutTitle'>Advisor at BrightID</div>
           <div data-aos="fade-right" data-aos-delay="600" className='aboutText aboutTagline' style={{
@@ -44,19 +68,19 @@ const About = () => {
           <a data-aos="flip-down" data-aos-delay="0" href='https://evanon.earth/brightid' target="_blank"
           className='aboutText aboutLink' id='brightidLink' onMouseOver={mouseover} onMouseLeave={mouseleave} style={{
             textDecoration: 'none',
-            color: '#7CE2F9'}}>evanon.earth/brightid -></a>
+            color: '#7CE2F9'}}>Learn More -></a>
           <br></br>
           <div data-aos="fade-right" data-aos-delay="900" className='aboutText aboutTitle'>Advisor at Unitap</div>
           <div data-aos="fade-right" data-aos-delay="1000" className='aboutText aboutTagline' style={{
             textDecoration: 'none',
-            color: '#ea49ff'}}>- Universal Token Distribution Platform -</div>
+            color: '#31a3ff'}}>- Universal Token Distribution Platform -</div>
           <div data-aos="fade-right" data-aos-delay="1100" className='aboutText' style={{
             textDecoration: 'none',
             color: '#bbbbbb'}}>Grow your dapp's userbase with free gas and token raffles:</div>
           <a data-aos="flip-down" data-aos-delay="00" href='https://evanon.earth/unitap' target="_blank"
           className='aboutText aboutLink' id='unitapLink' onMouseOver={mouseover} onMouseLeave={mouseleave} style={{
             textDecoration: 'none',
-            color: '#7CE2F9'}}>evanon.earth/unitap -></a>
+            color: '#7CE2F9'}}>Learn More -></a>
         </div>
       </div>
       <div className='aboutRightContainer'>
