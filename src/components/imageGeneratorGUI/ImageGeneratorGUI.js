58aa6eeba65bc42105b1ce18f8127f71ae9b740c
EvanGottschalk <magnus5557@gmail.com>
2025-01-27 10:15:01 -0500

    submit button fixes

--- src/components/imageGeneratorGUI/ImageGeneratorGUI.js
+++ src/components/imageGeneratorGUI/ImageGeneratorGUI.js
@@ -566,7 +566,7 @@ const ImageGeneratorGUI = () => {
           data-aos-delay={12 * delay_gap} data-aos="fade-right">
           Negative Prompt:
         </div>
-        <div className="imageGeneratorGUIsubmitButton imageGeneratorGUIfloatRightButton" id="copyNegativePromptButton" onMouseDown={handleMouseUp} onMouseUp={handleSubmitClick} data-aos-anchor-placement="top-center" data-aos-anchor="#anchorElement_ImageGeneratorGUI"
+        <div className="imageGeneratorGUIsubmitButton imageGeneratorGUIfloatRightButton imageGeneratorGUIcopyButton" id="copyNegativePromptButton" onMouseDown={handleMouseUp} onMouseUp={handleSubmitClick} data-aos-anchor-placement="top-center" data-aos-anchor="#anchorElement_ImageGeneratorGUI"
           data-aos-delay={12 * delay_gap} data-aos="fade-left">
           {mobile ? "Copy Default" : "Copy Default Negative Prompt"}
         </div>
